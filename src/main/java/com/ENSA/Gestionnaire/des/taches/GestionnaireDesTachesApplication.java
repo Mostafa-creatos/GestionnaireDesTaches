9152bc93cd2fd69a351949cec9b08ffadebf2cc0
Mostafa Faouzi <mostafafaouzi16@gmail.com>
2024-10-27 15:19:28 +0100

    first commit

---
+++ src/main/java/com/ENSA/Gestionnaire/des/taches/GestionnaireDesTachesApplication.java
@@ -0,0 +1,13 @@
+package com.ENSA.Gestionnaire.des.taches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionnaireDesTachesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionnaireDesTachesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ENSA/Gestionnaire/des/taches/GestionnaireDesTachesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ENSA.Gestionnaire.des.taches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionnaireDesTachesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
